3d077bfa4f37e2971963947411e17f771f063529
naegwon <joonwoo9192@gmail.com>
2024-04-25 23:52:52 +0900

    member domain 작성 member test 작성 item domain 작성

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,15 +21,15 @@ class MemberRepositoryTest {
     public void testMember() throws Exception{
         //given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
 
         //when
         Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
+        Member findMember = memberRepository.findOne(savedId);
 
         //then
         assertEquals(member.getId(), findMember.getId());
-        assertEquals(findMember.getUsername(), member.getUsername());
+        assertEquals(findMember.getName(), member.getName());
         assertEquals(findMember, member);
     }
     
