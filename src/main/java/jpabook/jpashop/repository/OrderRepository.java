678db68844e4c3610282eb6747003f7f904342fc
naegwon <joonwoo9192@gmail.com>
2024-05-15 23:16:07 +0900

    커밋

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -27,5 +28,39 @@ public class OrderRepository {
         return em.createQuery(jpql, Order.class)
                 .setMaxResults(1000)
                 .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o " +
+                        "join fetch o.member m " +
+                        "join fetch o.delivery d ",
+                Order.class
+        ).getResultList();
+    }
+
+    public List<OrderSimpleQueryDto> findOrderDto() {
+        //new 명령어를 사용해서 일반적인 SQL을 사용할 떄 처럼 원하는 값을 선택해서 조회
+        //JPQL의 결과를 DTO 즉시 전환
+        //네트워크 용량 최적화(생각보다 성능 차이 미비)
+        //리포지토리 재사용성 떨어짐, API 스펙에 맞춘 코드가 리포지토리에 들어가는 단점
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
+
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select o from Order o " +
+                    "join fetch o.member m " +
+                    "join fetch o.delivery d " +
+                    "join fetch o.orderItems oi " +
+                    "join fetch oi.item i ",
+                Order.class
+        ).getResultList();
     }
 }
